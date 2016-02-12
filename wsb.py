e6808e6e2b8cd72830b0e1dcb58099cd2353e2c9
Mike Cardillo <mcardillo55@gmail.com>
2016-02-12 02:42:27 -0800

    Fetch stock prices from yahoo, and display in color

--- wsb.py
+++ wsb.py
@@ -5,6 +5,7 @@ import re
 from collections import Counter
 import curses
 import datetime
+import yahoo_finance
 
 
 def printTable(stdscr, maxY, c):
@@ -12,8 +13,13 @@ def printTable(stdscr, maxY, c):
     stdscr.clear()
     for key, value in sorted(c.iteritems(), key=lambda (k,v): (v,k), reverse=True):
         if (y < maxY - 1):
-            stdscr.addstr(y, 0, key + "\t" + str(value)) 
-            y = y + 1
+            share = yahoo_finance.Share(key)
+            if share.get_price() is not None:
+                if share.get_change()[0] is '+':
+                    stdscr.addstr(y, 0, key + "\t" + str(value) + "\t" + str(share.get_price()) + "\t" + str(share.get_change() + "\t"), curses.color_pair(1))
+                else:
+                    stdscr.addstr(y, 0, key + "\t" + str(value) + "\t" + str(share.get_price()) + "\t" + str(share.get_change() + "\t"), curses.color_pair(2))
+                y = y + 1
     stdscr.addstr(maxY-1, 0, "Last updated: " + datetime.datetime.now().strftime("%m-%d-%Y %H:%M:%S"))
     stdscr.refresh()
 
@@ -21,6 +27,8 @@ def main(stdscr):
     EXCLUDE_LIST = ["I", "A", "YOLO", "IV", "US", "FUCK", "LOL", "RIP"]
 
     stdscr.nodelay(1)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_RED)
     stdscr.addstr(0,0,"Fetching data, please wait...")
     p = re.compile("\\b[^a-zA-Z0-9]?([A-Z]{1,4})[^a-zA-Z0-9]?\\b")
 
