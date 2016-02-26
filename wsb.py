8da18ab58e2fed57993886f674ac52d204f8fec4
Mike Cardillo <mcardillo55@gmail.com>
2016-02-26 03:40:01 -0800

    Moved the quote-fetching logic to its own function, rather than in the printTable function

--- wsb.py
+++ wsb.py
@@ -10,29 +10,44 @@ import yahoo_finance
 EXCLUDE_LIST = ["I", "A", "YOLO", "IV", "US", "FUCK", "LOL", "RIP"]
 
 
-def printTable(stdscr, maxY, numComments, c):
+def printTable(stdscr, maxY, numComments, shareData, c):
     global EXCLUDE_LIST
     y = 0
     stdscr.clear()
     for key, value in sorted(c.iteritems(), key=lambda (k,v): (v,k), reverse=True):
         if (y < maxY - 1):
-            try:
-                share = yahoo_finance.Share(key)
-            except AttributeError:
-                continue
-            if share.get_change() is None:
-            #Not a real symbol, remove and add to exclude list
-                EXCLUDE_LIST.append(key)
-                del c[key]
-                continue
-            elif share.get_change()[0] is '+':
+            if shareData[key].get_change()[0] is '+':
                 printColor = curses.color_pair(1)
             else:
                 printColor = curses.color_pair(2)
-            stdscr.addstr(y, 0, "%s\t%d\t%s\t%s\t" % (key, value, share.get_price(), share.get_change()), printColor)
+            stdscr.addstr(y, 0, "%s\t%d\t%s\t%s\t" % (key, value, shareData[key].get_price(), shareData[key].get_change()), printColor)
             y = y + 1
     stdscr.addstr(maxY-1, 0, "Last updated: %s\tComments parsed:%d" % (datetime.datetime.now().strftime("%m-%d-%Y %H:%M:%S"), numComments))
     stdscr.refresh()
+
+def getShareData(stdscr, num, c):
+    '''
+    Takes a counter c of symbols and counts and returns a dict with num amount
+    of symbol and Share object pairs. Also does error checking to detect false
+    symbols and removes them from c
+    '''
+    shareData = {}
+    y = 0 
+    for symbol, count in sorted(c.iteritems(), key=lambda (k,v): (v,k), reverse=True):
+        try:
+            share = yahoo_finance.Share(symbol)
+        except AttributeError:
+            continue
+        if share.get_change() is None:
+        #Not a real symbol, remove and add to exclude list
+            EXCLUDE_LIST.append(symbol)
+            del c[symbol]
+            continue
+        shareData[symbol] = share
+        if len(shareData) >= num:
+            break;
+        y = y + 1
+    return shareData
 
 def main(stdscr):
     global EXCLUDE_LIST
@@ -46,7 +61,6 @@ def main(stdscr):
     r = praw.Reddit("wallstreetbets comment parser")
     subreddit = r.get_subreddit("wallstreetbets")
 
-    maxY, maxX = stdscr.getmaxyx()
     c = Counter()
     commentsVisited = []
 
@@ -64,7 +78,8 @@ def main(stdscr):
                         if match not in EXCLUDE_LIST:
                             c.update([match])
                     commentsVisited.append(comment.id)
-        printTable(stdscr, maxY, len(commentsVisited), c)
+        shareData = getShareData(stdscr, maxY-1, c)
+        printTable(stdscr, maxY, len(commentsVisited), shareData, c)
         curses.napms(5000)
 
 curses.wrapper(main)
